81242808161e7a63cfbb0ee450adc86d6762cfdb
潘学森 <panxuesen@tojoy.com>
2022-09-22 20:29:21 +0800

    example

---
+++ example/index.tsx
@@ -0,0 +1,20 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+import PickerView from '../.';
+
+import { createRoot, Root } from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div>
+      <PickerView columns={[['1', '2'], ['a', 'b']]}/>
+    </div>
+  );
+};
+
+const container = document.getElementById('root')
+
+const root:Root = createRoot(container as HTMLElement)
+
+root.render(<App />);
